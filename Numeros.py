33bcfb007a7791b696f245144c28ce4bccd95b34
DaaaaaPriego <danibpriego2@gmail.com>
2024-05-28 12:30:27 -0600

    Ejercicio modificado a mátrices en personas

--- Numeros.py
+++ Numeros.py
@@ -1,14 +1,28 @@
-Digitos = []
+Nombres = []
 
-for i in range (3):
-    digito = input(f"Ingresa el dígito {i + 1}:" )
-    Digitos.append(digito)
+for z in range (3):
+    atributos = []
+    Sujeto = input(f"Ingresa el nombre {z + 1}:" )
+    atributos.append(Sujeto )
     
-if len(Digitos) == 3:
-    print("Los dígitos ingresados son:")
-    for digito in Digitos:
-        print(digito)
+    apellidoP = input(f"Ingresa el apellido paterno {z + 1}")
+    atributos.append(apellidoP )
+    
+    apellidoM = input(f"Ingresa el apellido materno {z + 1}")
+    atributos.append(apellidoM )
+    
+    Edad = input(f"Ingresa la edad {z + 1}")
+    atributos.append(Edad )
+    
+    Nombres.append(atributos)
+    print(Nombres)
+    
+if len(Nombres) == 3:
+    print("Los nombres ingresados son:")
+    for y in range(len(Nombres)):
+      for x in range(len(atributos)):
+        print(f"Sujeto{Nombres[y] [x]}:")
         
 else:
-    print("No se han ingresado suficientes dígitos")
+    print("No se han ingresado suficientes nombres")
     
